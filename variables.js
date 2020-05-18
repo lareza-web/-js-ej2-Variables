f7c36cbbd5a1bd62029b1bff29cba07bcfbc5a9c
lareaza-web <larezaweb@gmail.com>
2020-05-18 14:05:53 -0300

    variables

---
+++ variables.js
@@ -0,0 +1,19 @@
+' use strict '
+
+// VARIABLES
+// Una variable es un contenedor de información
+
+var pais = "España";
+var continente = "Europa";
+var antiguedad = 2019;
+var pais_y_continente = pais + " " + continente;
+let prueba = "hola";
+
+alert(prueba);
+
+antiguedad = 2030;
+alert(antiguedad);
+pais = "Venezuela";
+continente = "America";
+alert(pais_y_continente);
+console.log(pais, continente, antiguedad);
